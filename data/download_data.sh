db8b44eb734821a786196f2630a12ea1a69988c5
Elana Simon <elanasimon95@gmail.com>
2022-09-27 14:34:35 -0700

    fixed download script

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,13 +1,15 @@
-dataset = $1
+dataset=$1
 
-LINK='https://zenodo.org/record/6903423/files/${1}?download=1'
+LINK="https://zenodo.org/record/6903423/files/${dataset}"
 
+echo $LINK
 if [[ $LINK == *.pkl ]]; then
     mkdir datasets
     wget $LINK -O datasets/${dataset}
 else
-    if [[ $LINK == *.tar.gz ]]; then
+    if [[ $dataset == *.tar.gz ]]; then
+        echo "downloading ${dataset}..."
         wget $LINK
-        tar -xzvf $LINK
+        tar -xzvf $dataset
     fi
 fi
