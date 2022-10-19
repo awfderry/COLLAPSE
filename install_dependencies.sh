89da4b436122b273c303aabbdb0258b455a8572e
Rohan Koodli <rovik05@gmail.com>
2022-10-19 13:33:53 -0700

    spacing in install_dependencies, other changes

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,14 +7,16 @@ conda activate collapse
 # Install conda packages
 conda install pytorch==1.11.0 cudatoolkit=11.3 -c pytorch
 
+# Change CUDA and GPU to cpu if installing w/o gpu
 export TORCH=1.11.0
 export CUDA=cu113
+export GPU=gpu
 pip install torch-scatter -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
 pip install torch-sparse -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
-pip install torch-cluster-f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
+pip install torch-cluster -f https://data.pyg.org/whl/torch-${TORCH}+${CUDA}.html
 pip install torch-geometric
 
-conda install -c pytorch faiss-gpu
+conda install -c pytorch faiss-${GPU}
 
 # Install GVP
 git clone https://github.com/drorlab/gvp-pytorch
